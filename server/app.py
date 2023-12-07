f2c4ff4b8d8d756a058849200a00aa4e39adbd64
Sackett <skeesen8@SKLaptop.>
2023-12-07 10:00:01 -0600

    login function created

--- server/app.py
+++ server/app.py
@@ -3,15 +3,28 @@
 # Standard library imports
 
 # Remote library imports
-from flask import request
+from flask import request,make_response
 from flask_restful import Resource
 
 # Local imports
 from config import app, db, api
+from models import User
 # Add your model imports
 
 
 # Views go here!
+
+class Users(Resource):
+    def post(self):
+        data = request.get_json()
+        user = User(name=data['name'], username=data['username'], email = data['email'], password_hash = data['password'])
+        db.session.add(user)
+        db.session.commit()
+        return make_response({'user':user.to_dict()},201)
+    
+api.add_resource(Users, '/api/v1/users')
+
+
 
 @app.route('/')
 def index():
--- server/models.py
+++ server/models.py
@@ -1,6 +1,28 @@
 from sqlalchemy_serializer import SerializerMixin
 from sqlalchemy.ext.associationproxy import association_proxy
 
-from config import db
+from config import db,bcrypt
 
-# Models go here!
+class User(SerializerMixin, db.Model):
+    __tablename__ = 'users'
+
+    serialize_rules =('-password_hash',)
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String)
+    username = db.Column(db.String)
+    email = db.Column(db.String)
+    _password_hash = db.Column(db.String)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+
+    @property 
+    def password_hash(self):
+        return self._password_hash
+    
+    @password_hash.setter
+    def password_hash (self, plain_text_password):
+        byte_object = plain_text_password.encode('utf-8')
+        encrypted_password_object = bcrypt.generate_password_hash(byte_object)
+        hashed_password = encrypted_password_object.decode('utf-8')
+        self._password_hash = hashed_password
+
