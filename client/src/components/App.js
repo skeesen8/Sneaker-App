675ec0448cf49b5c00ed4d1b408346ebf5a509e5
Sackett <skeesen8@SKLaptop.>
2023-12-07 14:52:47 -0600

    rename and push to current

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,8 +1,63 @@
 import React, { useEffect, useState } from "react";
 import { Switch, Route } from "react-router-dom";
+import { ChakraProvider, RangeSliderProvider,Button,size } from '@chakra-ui/react'
+
+
+import Signup from "./Signup";
+
 
 function App() {
-  return <h1>Project Client</h1>;
-}
+  const [user,setUser]=useState(null)
+  
+  useEffect(() => {
+    fetch('/authorized')
+    .then((resp) => {
+      if (resp.ok) {
+        resp.json().then((user) => setUser(user))
+      } else {
+        console.log('error')
+      }
+    })
+  }, [])
+
+  function handleLogout(){
+    fetch("/logout",{
+      method:"DELETE"})
+      .then((resp)=>{
+        if(resp.ok){
+          setUser(null)
+        }
+      })
+    
+
+  }
+
+
+
+    if(!user){
+      return(
+
+      <ChakraProvider>
+        <Signup setUser={setUser}/>
+      </ChakraProvider>
+      )}
+
+      return(
+        <ChakraProvider>
+
+        <div>
+          in the site
+          <Button onClick={handleLogout} size="sm" colorScheme='teal'>Logout</Button>
+        </div>
+
+
+          
+        </ChakraProvider>
+
+      )
+    
+      
+    }
+    
 
 export default App;
